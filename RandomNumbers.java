dc1dd0eee74ae0b28154f9a241f976efc2d933a8
ShiviSaloni <salonishivi92@gmail.com>
2024-12-22 14:25:26 +0530

    committed successfully

---
+++ RandomNumbers.java
@@ -0,0 +1,18 @@
+package com.cdac.acts;
+
+public class RandomNumbers {
+
+	public static void main(String[] args) {
+		int a=(int) (1000+(10000-1000)*Math.random());
+		System.out.println(a);
+		a=(int) (1000+(10000-1000)*Math.random());
+		System.out.println(a);
+		a=(int) (1000+(10000-1000)*Math.random());
+		System.out.println(a);
+		a=(int) (1000+(10000-1000)*Math.random());
+		System.out.println(a);
+		a=(int) (1000+(10000-1000)*Math.random());
+		System.out.println(a);
+	}
+
+}
